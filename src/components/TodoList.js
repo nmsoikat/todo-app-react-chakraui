b8e78c228110c8a6d9d034c8cc9d1c40a1d18906
saykat1 <nursyakat@gmail.com>
2021-09-14 20:23:04 +0600

    to-do list scroll bar

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,4 @@
-import { VStack, HStack, Stack, Text, IconButton, Badge, Divider } from '@chakra-ui/react';
+import { Box, VStack, HStack, Stack, Text, IconButton, Badge, Divider } from '@chakra-ui/react';
 import { FaTrash, FaEdit } from 'react-icons/fa';
 import { useContext } from 'react';
 import { EditTodo } from '../App';
@@ -20,56 +20,58 @@ function TodoList({ todos, handelRemoveTodo }) {
 	}
 
 	return (
-		<VStack p="1" w="100%" alignItems="stretch">
-			{todos.map(function(todo) {
-				return (
-					<Stack
-						key={todo.id}
-						p="1"
-						borderRadius="lg"
-						border="1px"
-						borderColor={todo.priority ? 'pink.300' : 'gray.200'}
-					>
-						<Text
-							as="h3"
-							fontWeight="bold"
-							textTransform="capitalize"
-							color={todo.priority ? 'pink.500' : ''}
-						>
-							{todo.title}
-						</Text>
-						<Divider marginTop="4px !important" borderColor="gray.400" />
-						<Text marginTop="4px !important">{todo.detail}</Text>
-						<HStack
-							marginTop="0px !important"
-							textAlign="right"
+		<Box minW="100%" overflowY="auto" className="editScrollY">
+			<VStack p="1" minW="100%" alignItems="stretch" maxH="55vh">
+				{todos.map(function(todo) {
+					return (
+						<Stack
+							key={todo.id}
+							p="1"
 							borderRadius="lg"
-							padding="1"
-							w="100%"
-							justifyContent="space-between"
+							border="1px"
+							borderColor={todo.priority ? 'pink.300' : 'gray.200'}
 						>
-							<Badge colorScheme={todo.priority ? 'pink' : 'gray'} px="1" borderRadius="md">
-								{todo.priority ? 'High' : 'Normal'}
-							</Badge>
-							<HStack spacing="1">
-								<IconButton
-									color="blue.500"
-									icon={<FaEdit />}
-									onClick={() => handelEditTodo(todo.id)}
-									size="sm"
-								/>
-								<IconButton
-									color="red.500"
-									icon={<FaTrash />}
-									onClick={() => handelRemoveTodo(todo.id)}
-									size="sm"
-								/>
+							<Text
+								as="h3"
+								fontWeight="bold"
+								textTransform="capitalize"
+								color={todo.priority ? 'pink.500' : ''}
+							>
+								{todo.title}
+							</Text>
+							{todo.detail && <Divider marginTop="4px !important" borderColor="gray.400" />}
+							<Text marginTop="4px !important">{todo.detail}</Text>
+							<HStack
+								marginTop="0px !important"
+								textAlign="right"
+								borderRadius="lg"
+								padding="1"
+								w="100%"
+								justifyContent="space-between"
+							>
+								<Badge colorScheme={todo.priority ? 'pink' : 'gray'} px="1" borderRadius="md">
+									{todo.priority ? 'High' : 'Normal'}
+								</Badge>
+								<HStack spacing="1">
+									<IconButton
+										color="blue.500"
+										icon={<FaEdit />}
+										onClick={() => handelEditTodo(todo.id)}
+										size="sm"
+									/>
+									<IconButton
+										color="red.500"
+										icon={<FaTrash />}
+										onClick={() => handelRemoveTodo(todo.id)}
+										size="sm"
+									/>
+								</HStack>
 							</HStack>
-						</HStack>
-					</Stack>
-				);
-			})}
-		</VStack>
+						</Stack>
+					);
+				})}
+			</VStack>
+		</Box>
 	);
 }
 
